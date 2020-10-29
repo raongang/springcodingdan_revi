426c6e86e17cca03ed50a399de71a0432d4967c7
raongang <40487743+raongang@users.noreply.github.com>
2020-10-29 15:52:19 +0900

    2020-10-29 raongang CRUD

---
+++ jex02_revi/src/main/java/org/zerock/config/ServleConfig.java
@@ -0,0 +1,55 @@
+package org.zerock.config;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/*
+ *	spring5.0 이전에는 WebMvcConfigurer가 아닌  WebMvcConfigurerAdapter 추상클래스를 이용했음.
+*/
+
+@EnableWebMvc
+@ComponentScan(basePackages = { "com.zerock.controller" , "com.zerock.exception" })
+public class ServleConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.addResourceHandler("/resources/**").addResourceLocations("/ resources/");
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setViewClass(JstlView.class);
+		bean.setPrefix("/WEB-INF/views/");
+		bean.setSuffix(".jsp");
+		registry.viewResolver(bean);
+	}
+
+	/*
+	 *   CommonsMultipartResolver - 클래스
+	 *   MultipartResolver - Interface 
+	 */
+	@Bean(name="multipartResolver")
+	public CommonsMultipartResolver getResolver() throws IOException {
+		
+		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+		resolver.setDefaultEncoding("UTF-8");
+		resolver.setMaxUploadSize(104857560);
+		resolver.setMaxUploadSizePerFile(2097152);
+		resolver.setUploadTempDir(new FileSystemResource("c:\\upload\\tmp"));
+		resolver.setMaxInMemorySize(10485756);
+		
+		return resolver;
+	}
+}
---
+++ jex02_revi/src/main/java/org/zerock/config/WebConfig.java
@@ -0,0 +1,56 @@
+package org.zerock.config;
+
+import javax.servlet.Filter;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+// web.xml을 대신하는 config 파일
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] { RootConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {ServleConfig.class};
+	}
+	
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] { "/" } ;
+	}
+
+	//404 Error 예외처리 
+	@Override
+	protected void customizeRegistration(Dynamic registration) {
+		// TODO Auto-generated method stub
+		//super.customizeRegistration(registration);
+		registration.setInitParameter("throwExceptionIfNoHandlerFound", "true");
+	}
+	
+	// Filter 설정
+	@Override
+	protected Filter[] getServletFilters() {
+		
+		// TODO Auto-generated method stub
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return new Filter[] { characterEncodingFilter };
+	}
+	
+	
+	
+	
+	
+	
+	
+}
